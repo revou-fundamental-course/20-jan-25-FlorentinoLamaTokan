dfe735471981b1db80aee84fcc9632d95b5b2b45
Florentino Lama Tokan <ftino42@gmail.com>
2025-01-27 12:34:49 +0700

    create dokumentasi

--- script/script.js
+++ script/script.js
@@ -1,30 +1,33 @@
 function hitungBMI() {
-    
+    //mengambil value yang ada di html dan masukkan ke variable js
     const tinggiBadan = parseFloat(document.getElementById('tinggiBadan').value);
     const beratBadan = parseFloat(document.getElementById('beratBadan').value);
 
+    //proteksi agar user mengisi nilai dengan benar
     if (isNaN(beratBadan) || isNaN(tinggiBadan) || beratBadan <= 0 || tinggiBadan <= 0) {
         document.getElementById('hasil').innerText = "Harap masukkan nilai yang valid.";
         return;
     }
 
     
-
+    //rumus untuk kalkulator bmi
     const bmi = beratBadan / (tinggiBadan * tinggiBadan);
     let category = "";
 
+    //menggunakan ifStatement agar bisa mengkategorikan hasil yang akan diperoleh
     if (bmi < 18.5) {
-        category = "Kurus (Underweight)";
+        category = "Kekurangan berat badan";
     } else if (bmi >= 18.5 && bmi < 24.9) {
         category = "Normal (Ideal)";
     } else if (bmi >= 25 && bmi < 29.9) {
-        category = "Gemuk (Overweight)";
+        category = "Kelebihan berat badan";
     } else {
-        category = "Obesitas";
+        category = "Kegemukan(Obesitas)";
     }
 
+    //memanggil id hasil untuk menampilkan hasil yang diperoleh dari perhitungan
     document.getElementById('hasil').innerHTML = `
         <p>BMI Anda: <strong>${bmi.toFixed(2)}</strong></p>
-        <p>Kategori: <strong>${category}</strong></p>
+        <p>Status: <strong>${category}</strong></p>
     `;
 }
