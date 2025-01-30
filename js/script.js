dc45c7ec8b002ba028ac36893f7ee836a8a4b49f
Florentino Lama Tokan <ftino42@gmail.com>
2025-01-30 12:11:40 +0700

    update article and tips

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function hitungBMI() {
 
     //proteksi agar user mengisi nilai dengan benar
     if (isNaN(beratBadan) || isNaN(tinggiBadan) || beratBadan <= 0 || tinggiBadan <= 0) {
-        document.getElementById('hasil').innerText = "Harap masukkan nilai yang valid.";
+        document.getElementById('hasil').innerText = "Harap masukkan nilai yang valid!";
         return;
     }
 
@@ -13,21 +13,33 @@ function hitungBMI() {
     //rumus untuk kalkulator bmi
     const bmi = beratBadan / (tinggiBadan * tinggiBadan);
     let category = "";
+    let tips = "";
 
     //menggunakan ifStatement agar bisa mengkategorikan hasil yang akan diperoleh
     if (bmi < 18.5) {
         category = "Kekurangan berat badan";
+        tips = '<p>1. Konsumsi Kalori Lebih banyak<p/><br><p>2. Pilih Makanan Bernutrisi Tinggi<p/><br><p>3. Makan Lebih Sering<p/><br>';
     } else if (bmi >= 18.5 && bmi < 24.9) {
         category = "Normal (Ideal)";
+        tips = '<p>1. Atur Pola Makan dengan Gizi Seimbang<p/><br><p>2. Makan dalam Porsi Cukup & Teratur<p/><br><p>3. Berolahraga minimal seminggu 3 kali<p/><br>';
     } else if (bmi >= 25 && bmi < 29.9) {
         category = "Kelebihan berat badan";
+        tips = '<p>1. Pilih Makanan Bergizi & Seimbang<p/><br><p>2. Makan dalam Porsi yang Tepat<p/><br><p>3. Rutin Berolahraga<p/><br>';
     } else {
         category = "Kegemukan(Obesitas)";
+        tips = '<p>1. Pilih Makanan Bergizi & Seimbang<p/><br><p>2. Makan dalam Porsi yang Tepat<p/><br><p>3. Rutin Berolahraga<p/><br>';
     }
+
 
     //memanggil id hasil untuk menampilkan hasil yang diperoleh dari perhitungan
     document.getElementById('hasil').innerHTML = `
         <p>BMI Anda: <strong>${bmi.toFixed(2)}</strong></p>
         <p>Status: <strong>${category}</strong></p>
+        
+    `;
+
+    document.getElementById('tips').innerHTML = `
+        <p>Tips: <strong>${tips}</strong></p>
+        
     `;
 }
